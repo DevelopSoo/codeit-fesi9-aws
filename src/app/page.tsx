c0b2f7e0247a021b6429e9d9c7dbb7666ccd9685
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2025-05-14 11:30:30 +0900

    도커배포

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
 
   return (
     <div>
-      <div>한국어: 안녕 자동 CodeDeploy 배포야! 2</div>
+      <div>한국어: 안녕 자동 도커 배포야! </div>
       <div>영어: {message}</div>
     </div>
   );
